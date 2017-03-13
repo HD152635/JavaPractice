5933048803c4af0668f619c54df659d688a671b8
HD152635 <ha819ha@gmail.com>
2017-03-13 19:17:15 +0900

    Update Operator.java

--- JavaPractice/src/org/dimigo/basic/Operator.java
+++ JavaPractice/src/org/dimigo/basic/Operator.java
@@ -12,7 +12,7 @@ package org.dimigo.basic;
  * 2. 작성일 : 2017. 3. 13.
  * </pre>
  *
- * @author      : 공경배
+ * @author      : 
  * @version     : 1.0
  */
 public class Operator {
@@ -21,18 +21,15 @@ public class Operator {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int salary = 1700000;
-		int employee = 3;
-		int store = 1500;
-		long sum=((long)salary*(long)employee*(long)store*12);
+		int salary = 1700000,emp = 3,store = 1500;
+		long sum=(long)salary*(long)emp*(long)store*12L;
 		
 		System.out.println("<< 디미베네 연간 인건비 >>");
+		System.out.println("월 평균 급여 :" + String.format("%,d원", salary));
+		System.out.println("점포 내 직원 수 :" + String.format("%,d명", emp));
+		System.out.println("점포 수 :" + String.format("%,d개\n", store));
 		
-		System.out.println("월 평균 급여 :" + String.format("%,d", salary)+"원");
-		System.out.println("점포 내 직원 수 :" + String.format("%,d", employee)+"명");
-		System.out.println("점포 수 :" + String.format("%,d", store)+ "개");
-		
-		System.out.println("\n연간 인건비 :" + String.format("%,d", sum)+ "원");
+		System.out.println("연간 인건비 :" + String.format("%,d", sum));
 	}
 
 }
